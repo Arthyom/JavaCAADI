7501ecaa220c7cf8ced3a90128f307cbf32b21b2
arthyom <allpipiasaaa@hotmail.com>
2017-03-24 09:16:35 -0600

    El EJB, puente entre el cliente y el servidor

---
+++ EjemplosJavaEE/HolaJEE/src/java/Pruebas/Prueba.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pruebas;
+
+import beans.EjbSesion;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+
+
+
+/**
+ *
+ * @author frodo
+ */
+public class Prueba {
+    
+    
+    public static EJBContainer contenedor;
+    public static Context contexto;
+    public static EjbSesion ejb1;
+    
+    public static void main(String[] args)throws Exception{
+        
+        iniciarContenedor();
+        
+        
+        
+    }
+    
+    public static void iniciarContenedor ()throws Exception
+    {
+        contenedor = EJBContainer.createEJBContainer();
+        contexto = contenedor.getContext();
+        ejb1 = (EjbSesion) contexto.lookup("java:global/classes/EjbSesion!beans.EjbSesion");
+        int c = ejb1.sumar(1, 4);
+        System.out.println(c);;
+    }
+    
+    
+    
+
+    
+    
+    
+}
