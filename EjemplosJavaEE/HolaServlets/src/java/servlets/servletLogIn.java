1dfaeed1672c60bc6d970608023dd312da273350
arthyom <allpipiasaaa@hotmail.com>
2017-03-26 17:13:04 -0600

    agregando un log in bonito

--- EjemplosJavaEE/HolaServlets/src/java/servlets/servletLogIn.java
+++ EjemplosJavaEE/HolaServlets/src/java/servlets/servletLogIn.java
@@ -12,6 +12,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
 
 /**
  *
